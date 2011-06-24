b69b3e904127e3fa22d6306a5b9d2fb6751b6b3d
Peter Danenberg <pcd@roxygen.org>
2011-06-24 03:27:55 -0700

    TODO: Stability; get rid of old version of prune.conditional.independencies.

--- influence.R
+++ influence.R
@@ -51,160 +51,6 @@ for.each.adjacent.pair <- function(graph, f) {
         f(graph, whence, whither)
 }
 
-prune.conditional.independencies <- function(graph, data, cardinality=0) {
-  there.exist.adjacent.nodes.of.sufficient.cardinality <- FALSE
-
-  for.each.adjacent.pair(graph, function(graph, whence, whither) {
-    callCC(function(k) {
-    debug(cardinality, whence, whither)
-    ## This version calculates all paths from whence to whither:
-    ## 
-    ## nodes <- setdiff(intersect(union(graph[[whence]], graph[[whither]]),
-    ##                            nodes.from(graph, whence, whither)),
-    ##                  c(whence, whither))
-    ##
-    ## This is the Kalisch-version, dealing only with adjacency:
-    ## 
-    nodes <- setdiff(union(graph[[whence]], graph[[whither]]),
-                     c(whence, whither))
-    if (length(nodes) >= cardinality) {
-      ## debug(1)
-      there.exist.adjacent.nodes.of.sufficient.cardinality <<- TRUE
-      for (m in cardinality:length(nodes)) {
-        ## debug(2)
-        if (m == 0) {
-          ## debug(2.5)
-          if (cor.test(data[,whence], data[,whither])$p.value < 0.05) {
-            graph[[whence]] <- setdiff(graph[[whence]], whither)
-            graph[[whither]] <- setdiff(graph[[whither]], whence)
-            break
-          }          
-        } else {
-          ## Does failure of lm or cor.test to return imply an
-          ## orthogonality such that we can break the dependence? That
-          ## would be nice.
-          ##
-          ## lapply-based parallelization here could create a
-          ## combination -> indepedence map, which we could then use
-          ## to break links.
-          ##
-          ## Is it problematic, though, that we mutate the graph in
-          ## real-time? Possibly.
-          ##
-          ## Let's throw a big machine at it and see what happens.
-          ## debug(2.9)
-          ## debug(nodes, m)
-          ## Aha: this becomes prohibitively expensive; can we stream
-          ## this?
-          ## debug(combn(nodes, m, FUN=function(shit) NULL, simplify=FALSE))
-          ## Need some kind of combinatorial for-each that discards
-          ## its value.
-          ##
-          ## Let this be a lesson, though: don't gather where we're
-          ## only shooting for side-effect.
-          ##
-          ## Combinadics:
-          ## http://www.markmfredrickson.com/thoughts/2010-08-06-combinadics-in-r.html
-          ## http://stats.stackexchange.com/questions/1286/how-can-i-obtain-some-of-all-possible-combinations-in-r
-          ## http://compprog.wordpress.com/2007/10/17/generating-combinations-1/
-          ## https://stat.ethz.ch/pipermail/r-help/2006-October/115012.html
-          ## http://www.omegahat.org/Combinations/
-          debug('YYYYYYYYYY', nodes, m)
-          ## callCC(function(k)
-                 combinations(length(nodes), m, function(x) {
-          ## for (subset in combn(nodes, m)) {
-            subset <- nodes[as.logical(x)]
-            ## debug(subset)
-            ## debug(subset)
-            ## debug(3)
-            resid.whence <-
-              tryCatch(resid(lm(with(data,
-                                     as.formula(sprintf("%s ~ %s",
-                                                        whence,
-                                                        paste(subset, collapse='+')))))),
-                       error=function(e) {
-                         ## warning(sprintf('lm failed on whence: %s; breaking.',
-                         ##                 conditionMessage(e)),
-                         ##         immediate.=TRUE)
-                         NA
-                       });
-            if (is.na(resid.whence)) {
-              ## debug('harro')
-              ## graph[[whence]] <<- setdiff(graph[[whence]], whither)
-              ## graph[[whither]] <<- setdiff(graph[[whither]], whence)
-              k(NULL)
-              return()
-            }
-            
-            ## debug(4)
-            resid.whither <-
-              tryCatch(resid(lm(with(data,
-                                     as.formula(sprintf("%s ~ %s",
-                                                        whither,
-                                                        paste(subset, collapse='+')))))),
-                       error=function(e) {
-                         ## warning(sprintf('lm failed on whither: %s; breaking.',
-                         ##                 conditionMessage(e)),
-                         ##         immediate.=TRUE)
-                         NA
-                       });
-            if (is.na(resid.whither)) {
-              ## debug('harro')
-              ## graph[[whence]] <<- setdiff(graph[[whence]], whither)
-              ## graph[[whither]] <<- setdiff(graph[[whither]], whence)
-              k(NULL)
-              return()
-            }
-            
-            ## debug(5)
-            ## Can't yet get these to work:
-            ## 
-            ## lm.fit(y=as.matrix(data)[,c(whence, whither)],
-            ##        x=cbind(1, as.matrix(data[,nodes])))
-            ## 
-            ## lm.fit(y=cor(data[,c(whence, whither)]),
-            ##        x=cor(data[,nodes]))
-            length <- min(length(resid.whither), length(resid.whence))
-
-            ## We might be able to fuck with `method' and `exact' here
-            ## to avoid the NA p-value problem.
-            ##
-            ## We have another problem, though, of insufficient finite
-            ## observations roughly 10 mins in.
-            p.value <-
-              tryCatch(cor.test(sample(resid.whence, length),
-                                sample(resid.whither, length))$p.value,
-                       error=function(e) {
-                         warning(sprintf('cor.test failed: %s; breaking.',
-                                         conditionMessage(e)),
-                                 immediate.=TRUE)
-                         NA
-                       });
-
-            ## What are we saying here? I.e. should we delete the edge
-            ## or not?
-            if (is.na(p.value)) {
-              ## warning('p.value is NA: breaking.', immediate.=TRUE)
-              ## graph[[whence]] <<- setdiff(graph[[whence]], whither)
-              ## graph[[whither]] <<- setdiff(graph[[whither]], whence)
-              k(NULL)
-            } else if (p.value < 0.05) {
-              debug('XXXXXXXXXXXXX', whence, whither)
-              graph[[whence]] <<- setdiff(graph[[whence]], whither)
-              graph[[whither]] <<- setdiff(graph[[whither]], whence)
-              ## XXX: need to break out of combinations here!
-              k(NULL)
-            }
-          })
-          ## )
-        }
-      }
-    }
-  })})
-    if (there.exist.adjacent.nodes.of.sufficient.cardinality)
-      prune.conditional.independencies(graph, data, cardinality + 1)
-}
-
 prune.conditional.independencies <- function(graph,
                                              data,
                                              alpha=0.05,
@@ -215,40 +61,23 @@ prune.conditional.independencies <- function(graph,
   for.each.adjacent.pair(graph, function(graph, whence, whither) {
     callCC(function(k) {
       debug(cardinality, whence, whither)
+      ## Spirtes:
+      ## 
       ## nodes <- setdiff(intersect(union(graph[[whence]], graph[[whither]]),
       ##                            nodes.from(graph, whence, whither)),
       ##                  c(whence, whither))
+      ##
+      ## Kalisch:
+      ## 
       nodes <- setdiff(union(graph[[whence]], graph[[whither]]),
                        c(whence, whither))
-      ## Damn: we need to recalculate nodes after every change in the
-      ## graph. We do that, don't we (cf. callCC)?
-      ##
-      ## The other problem is that, after deleting an edge,
-      ## for.each.adjacent.pair should be adjusted; how to prevent it
-      ## from starting from scratch, though?
-      ##
-      ## That's solved, at least, by the is.adjacent check in
-      ## for.each.adjacent.pair (whew!).
-      ##
+
       ## We might have to sample this fucking space and resort to
       ## combinadics, after all.
-      ##
-      ## Or, as a heuristic, can we specify some maximum
-      ## subset-cardinality?
-      ##
-      ## Why not give the choice between
-      ## sampling/max-subset-cardinality? They're both a form of
-      ## sampling, aren't they?
-      ##
-      ## Memoization?
       ##
       ## k(NULL) on lm-failure is particularly pessimistic; we can
       ## probably afford to do some sampling here, since some
       ## lm-failures still result in pruning.
-      ##
-      ## Would we could fucking prune the lm-failures.
-      ##
-      ## Wow: unevidenced graphs still settle.
       if (length(nodes) >= cardinality) {
         there.exist.adjacent.nodes.of.sufficient.cardinality <<- TRUE
         for (m in cardinality:length(nodes)) {
@@ -261,6 +90,7 @@ prune.conditional.independencies <- function(graph,
                                  immediate.=TRUE)
                          NA
                        })
+            ## This is questionable, isn't it: pruning on NA?
             if (is.na(p.value) || p.value < alpha) {
               debug(p.value)
               graph[[whence]] <- setdiff(graph[[whence]], whither)
@@ -271,7 +101,6 @@ prune.conditional.independencies <- function(graph,
             debug(m, length(nodes))
             combinations(length(nodes), m, function(x) {
               subset <- nodes[as.logical(x)]
-              ## debug(subset, vec.length=20)
               resid.whence <-
                 tryCatch(resid(lm(with(data,
                                        as.formula(sprintf("%s ~ %s",
@@ -284,11 +113,8 @@ prune.conditional.independencies <- function(graph,
                                    immediate.=TRUE)
                            NA
                          });
-              if (is.na(resid.whence)) {
-                ## graph[[whence]] <- setdiff(graph[[whence]], whither)
-                ## graph[[whither]] <- setdiff(graph[[whither]], whence)
+              if (is.na(resid.whence))
                 k(NULL)
-              }
 
               resid.whither <-
                 tryCatch(resid(lm(with(data,
@@ -302,11 +128,8 @@ prune.conditional.independencies <- function(graph,
                                    immediate.=TRUE)
                            NA
                          });
-              if (is.na(resid.whither)) {
-                ## graph[[whence]] <- setdiff(graph[[whence]], whither)
-                ## graph[[whither]] <- setdiff(graph[[whither]], whence)
+              if (is.na(resid.whither))
                 k(NULL)
-              }
               
               length <- min(length(resid.whither), length(resid.whence))
 
@@ -320,6 +143,7 @@ prune.conditional.independencies <- function(graph,
                            NA
                          });
 
+              ## Vide supra.
               if (is.na(p.value) || p.value < alpha) {
                 debug(p.value, subset)
                 graph[[whence]] <<- setdiff(graph[[whence]], whither)
